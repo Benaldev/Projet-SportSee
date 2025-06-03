b79116426d6a9ad7a2750470c709bc83fe282c75
Walid <walid.bensalah20@gmail.com>
2025-06-03 16:14:55 +0200

    comingsoon page

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./styles.scss";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="under-construction">
+      <h1> Page en cours de construction. </h1>
+      <p>
+        Nous travaillons activement pour vous offrir une meilleure expérience.
+      </p>
+      <p>Revenez bientôt !</p>
+    </div>
+  );
 };
 
 export default Settings;
