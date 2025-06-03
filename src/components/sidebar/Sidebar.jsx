ab23773e64db601892b7c05694cab8b500ff8436
Walid <walid.bensalah20@gmail.com>
2025-06-03 15:53:07 +0200

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import yogaIcon from "../../assets/images/yogaIcon.png";
+import swimIcon from "../../assets/images/swimIcon.png";
+import bikeIcon from "../../assets/images/bikeIcon.png";
+import liftIcon from "../../assets/images/liftIcon.png";
+import "./styles.scss";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li>
+          <img src={yogaIcon} alt="Yogo icon" />
+        </li>
+        <li>
+          <img src={swimIcon} alt="Swim icon" />
+        </li>
+        <li>
+          <img src={bikeIcon} alt="Bike icon" />
+        </li>
+        <li>
+          <img src={liftIcon} alt="Weight lifting icon" />
+        </li>
+      </ul>
+      <p className="copyright">Copyright, SportSee 2025</p>
+    </aside>
+  );
 };
 
 export default Sidebar;
